65f63e595774c1d3c0af15266f4df17d643c8b3f
SriVenkat77 <167739518+SriVenkat77@users.noreply.github.com>
2024-08-31 17:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,21 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 
 
-const dbURI = process.env.MONGODB_URI;
+// Connection string
+const dbURI = process.env.MONGODB_URI; // Ensure this environment variable is set
 
 if (!dbURI) {
     console.error('MongoDB connection error: Missing MONGODB_URI');
-    process.exit(1); // Exit the application if no URI is provided
+    process.exit(1); // Exit if URI is missing
 }
 
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // 30 seconds
+    serverSelectionTimeoutMS: 30000 // Increase the timeout to 30 seconds
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
-// Export the connection object to use in other modules
-module.exports = dbConnection;
 
 app.use(express.json());
 
