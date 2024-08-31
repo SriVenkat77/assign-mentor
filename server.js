6949da92fb15b152a453d3c1821069d805883bb7
SriVenkat77 <venkateshwaran1112@gmail.com>
2024-08-31 15:45:49 +0530

    MENTOR-STUDENT-API

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
